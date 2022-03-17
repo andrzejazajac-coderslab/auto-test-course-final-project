4d96c3c19b45db0eaa06d3437d3a80e45d55113b
Andrzej <andrzejazajac+coderslab@gmail.com>
2022-03-17 17:06:04 +0100

    Add step "Add to login page"

--- src/cucumber/features/myStore-setAndCheckAdress.feature
+++ src/cucumber/features/myStore-setAndCheckAdress.feature
@@ -2,6 +2,7 @@ Feature: Login user in mystore, set address and check address
 
   Scenario Outline: User login to https://mystore-testlab.coderslab.pl, set and check user address
 	Given Page https://mystore-testlab.coderslab.pl open in browser <browser>
+	And Go to login page
 
 
 	Examples:
--- src/test/java/pl/coderslab/automationtestercourse/finalproject/MyStoreSetAndCheckAddressSteps.java
+++ src/test/java/pl/coderslab/automationtestercourse/finalproject/MyStoreSetAndCheckAddressSteps.java
@@ -1,6 +1,6 @@
 package pl.coderslab.automationtestercourse.finalproject;
 
-import io.cucumber.java.en.Given;
+import io.cucumber.java.en.*;
 import org.openqa.selenium.WebDriver;
 
 import java.time.Duration;
@@ -8,11 +8,18 @@ import java.time.Duration;
 public class MyStoreSetAndCheckAddressSteps {
 
     private WebDriver driver;
+    private MyStoreNavElement msNavElement;
 
     @Given("^Page (.*) open in browser (.*)$")
     public void pageOpenInBrowser(String pageUrl, String browser)
     {
         driver = TestHelper.beforeEach(driver,browser, Duration.ofSeconds(8));
         driver.get(pageUrl);
+    }
+
+    @And("Go to login page")
+    public void goToLoginPage() {
+        this.msNavElement = new MyStoreNavElement (this.driver);
+        this.msNavElement.clickUserIcon();
     }
 }
