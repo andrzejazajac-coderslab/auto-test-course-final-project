226a09e22d419b556166ab04ff14169c75749a3a
Andrzej <andrzejazajac+coderslab@gmail.com>
2022-03-17 15:59:02 +0100

    Add MyStoreHomePage class.

---
+++ src/test/java/pl/coderslab/automationtestercourse/finalproject/MyStoreHomePage.java
@@ -0,0 +1,11 @@
+package pl.coderslab.automationtestercourse.finalproject;
+
+import org.openqa.selenium.WebDriver;
+
+public class MyStoreHomePage extends PageObject{
+
+    public MyStoreHomePage(WebDriver driver) {
+        super(driver);
+    }
+
+}
