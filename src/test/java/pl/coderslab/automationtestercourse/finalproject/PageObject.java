16950c5da9bd2e5d55d27d179b45dc77246e6e75
Andrzej <andrzejazajac+coderslab@gmail.com>
2022-03-17 14:07:46 +0100

    Add helper class to Page Object Pattern(Model).

---
+++ src/test/java/pl/coderslab/automationtestercourse/finalproject/PageObject.java
@@ -0,0 +1,13 @@
+package pl.coderslab.automationtestercourse.finalproject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+
+public class PageObject {
+
+    public PageObject(WebDriver driver){
+    PageFactory.initElements(driver, this);
+    }
+
+}
