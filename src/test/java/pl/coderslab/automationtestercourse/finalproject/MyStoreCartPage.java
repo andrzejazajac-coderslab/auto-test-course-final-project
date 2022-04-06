89c2cd5e5dc4cd927eda028f35a628837e8270d3
Andrzej <andrzejazajac+coderslab@gmail.com>
2022-04-06 19:59:57 +0200

    Add MyStoreCartPage.java

---
+++ src/test/java/pl/coderslab/automationtestercourse/finalproject/MyStoreCartPage.java
@@ -0,0 +1,10 @@
+package pl.coderslab.automationtestercourse.finalproject;
+
+import org.openqa.selenium.WebDriver;
+
+public class MyStoreCartPage extends PageObject{
+
+    public MyStoreCartPage(WebDriver driver) {
+        super(driver);
+    }
+}
