88761e6ccfd242a03210460ce5e8bbefa27702d7
Andrzej <andrzejazajac+coderslab@gmail.com>
2022-04-06 16:28:47 +0200

    Add MyStoreProductPage.java

---
+++ src/test/java/pl/coderslab/automationtestercourse/finalproject/MyStoreProductPage.java
@@ -0,0 +1,10 @@
+package pl.coderslab.automationtestercourse.finalproject;
+
+import org.openqa.selenium.WebDriver;
+
+public class MyStoreProductPage extends PageObject{
+
+    public MyStoreProductPage(WebDriver driver) {
+        super(driver);
+    }
+}
